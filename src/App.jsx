3524dd357574d5218b177c3685e6f8268f79b4d1
Joseph Buchholz <70069846+JosephBuchholz@users.noreply.github.com>
2024-08-20 20:19:54 -0700

    Basic setup of React and Tailwind CSS (with Vite)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="font-semibold bg-blue-400">Hello</p>
+    </>
+  );
+}
+
+export default App;
